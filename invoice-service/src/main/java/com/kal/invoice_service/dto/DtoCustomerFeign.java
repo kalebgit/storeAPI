5ac676aac1ce6718576fa584aea393e710dbf34d
Kaleb Jiménez <emilianokaleb@gmail.com>
2026-06-05 17:39:54 -0600

    fix(): se arreglo algo relacionado a la facturacion

--- invoice-service/src/main/java/com/kal/invoice_service/dto/DtoCustomerFeign.java
+++ invoice-service/src/main/java/com/kal/invoice_service/dto/DtoCustomerFeign.java
@@ -1,11 +1,15 @@
 package com.kal.invoice_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DtoCustomerFeign {
+    @JsonAlias("customerId")
     private Integer id;
     private String name;
     private String email;
