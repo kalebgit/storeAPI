233306107c8dee262d217cc3c97c1a0136e4d327
Kaleb Jiménez <emilianokaleb@gmail.com>
2026-06-05 17:55:35 -0600

    feat(): correccion para el mapper de customer

--- customer-service/src/main/java/com/kal/customer_service/mapper/MapperCustomer.java
+++ customer-service/src/main/java/com/kal/customer_service/mapper/MapperCustomer.java
@@ -4,9 +4,11 @@ package com.kal.customer_service.mapper;
 import com.kal.customer_service.dto.DtoCustomerFeign;
 import com.kal.customer_service.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MapperCustomer {
 
+    @Mapping(source = "id", target = "customerId")
     DtoCustomerFeign customerToDtoCustomerFeign(Customer customer);
 }
