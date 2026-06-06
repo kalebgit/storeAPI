629532599f8400b404f72c4e8d0121c885abcb0a
Kaleb Jiménez <emilianokaleb@gmail.com>
2026-06-05 18:13:07 -0600

    fix(): se agrego un constructor

--- invoice-service/src/main/java/com/kal/invoice_service/dto/DtoProductOut.java
+++ invoice-service/src/main/java/com/kal/invoice_service/dto/DtoProductOut.java
@@ -3,9 +3,12 @@ package com.kal.invoice_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DtoProductOut {
 
     private Integer productId;
